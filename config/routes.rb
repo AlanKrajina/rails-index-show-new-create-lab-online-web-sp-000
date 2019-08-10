f7af6e2e0c44def2cdff6133ffa34e6cd471c89d
Alan Krajina <alankrajina88@gmail.com>
2019-08-10 10:35:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :coupons, only: [:index, :new, :create]
+  resources :coupons, only: [:index, :new, :create, :show]
   get '/student/:id', to: 'students#show', as: 'student'
 
 end
